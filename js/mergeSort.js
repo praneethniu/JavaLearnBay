da396286facb4755ffc8a3c73f5442adaa85701e
Tangallapally <praneeth.tangallapal@dish.com>
2020-06-07 14:50:00 -0600

    [][][PATCH] more changes

--- js/mergeSort.js
+++ js/mergeSort.js
@@ -1,49 +1,50 @@
-const merge = (first, second) => {
-  let i = 0
-  let j = 0
+const merge = (arr, strt, mid, end) => {
+  let i = strt,
+    j = mid + 1 // i = 0,  j= 4
+  let tempArray = [] // size = 8  {1, 2, 3, 4, 5, 6, 7, 8}
   let k = 0
-  const temp = []
-  while (i < first.length && j < second.length) {
-    if (first[i] > second[j]) {
-      temp[k] = second[j]
-      j++
+
+  while (i <= mid && j <= end) {
+    if (arr[i] < arr[j]) {
+      tempArray[k] = arr[i]
       k++
-    } else {
-      temp[k] = first[i]
       i++
+    } else {
+      tempArray[k] = arr[j]
       k++
+      j++
     }
   }
 
-  while (i < first.length) {
-    temp[k] = first[i]
+  while (i <= mid) {
+    tempArray[k] = arr[i]
+    k++
     i++
-    k++
-  }
-  while (j < second.length) {
-    temp[k] = second[j]
-    j++
-    k++
-  }
-  return temp
-}
-const mergeSort = (arr) => {
-  const splitArray = (arr, start, end) => {
-    if (start == end) {
-      return
-    }
-    if (start < end) {
-      const mid = Math.floor((start + end) / 2) + 1
-      const first = splitArray(arr, start, mid - 1)
-      const second = splitArray(arr, mid, end)
-      const mergedArray = merge(first, second)
-      return mergedArray
-    }
   }
 
-  return splitArray(arr, 0, arr.length - 1)
+  while (j <= end) {
+    tempArray[k] = arr[j]
+    k++
+    j++
+  }
+
+  j = strt
+  for (i = 0; i < end - strt + 1; i++) {
+    arr[j] = tempArray[i]
+    j++
+  }
+}
+
+const divideArray = (arr, strt, end) => {
+  if (strt == end) return
+  if (strt < end) {
+    let mid = Math.floor((strt + end) / 2)
+    divideArray(arr, strt, mid)
+    divideArray(arr, mid + 1, end)
+    merge(arr, strt, mid, end)
+  }
 }
 
 const x = [12, 34, 5, 78, 9, 9, 1, 2, 74]
-
-console.log(mergeSort(x))
+divideArray(x, 0, x.length-1)
+console.log(x)
